7aa38025e1da01e61ec1a17ac3851e88700fd2d0
Jason Froehlich <jfroehli@localhost.localdomain>
2021-01-06 15:38:46 -0500

    Updated script and workflow

--- bin/git-push.sh
+++ bin/git-push.sh
@@ -2,6 +2,9 @@
 
 echo ${1}
 
+git config user.name "Argo"
+git config user.email "you@example.com"
+
 echo "Hello World! Argo Vault example" >> output.txt
 git add .
 git commit -m "Added my Argo Output"
